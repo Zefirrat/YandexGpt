76fe73b0e6d50162bfa7ffce5fa968c844a24fa5
Павлов Леонид <lpavlov@nvtech.ru>
2023-12-14 20:58:45 +0500

    implementation

--- Zefirrat.YandexGpt.Prompter/YaPrompter.cs
+++ Zefirrat.YandexGpt.Prompter/YaPrompter.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Zefirrat.YandexGpt.Abstractions;
 using Zefirrat.YandexGpt.Api.Client;
+using Zefirrat.YandexGpt.Api.Contracts.Secondary;
 using Zefirrat.YandexGpt.Base;
 
 namespace Zefirrat.YandexGpt.Prompter
@@ -18,6 +19,14 @@ namespace Zefirrat.YandexGpt.Prompter
         public async Task<string> SendAsync(string message, CancellationToken cancellationToken = default)
         {
             return await SendBaseAsync(message, cancellationToken);
+        }
+
+        public async Task<string> SendAsync(string message, int tokens, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var request = CreateRequest(message);
+            request.CompletionOptions.MaxTokens = tokens.ToString();
+            return (await YaClient.SendAsync(request, cancellationToken)).Result.Alternatives[0].Message.Text;
+            
         }
     }
 }
